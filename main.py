6c16ab21dc7f392be01c56f33d07347926aa5477
Alex Vig <jalexvig@gmail.com>
2025-07-31 00:37:33 -0500

    Make foreign lang changeable

--- main.py
+++ main.py
@@ -3,53 +3,77 @@ import re
 import subprocess
 import tempfile
 from pathlib import Path
+from typing import Iterable, Dict
 
 os.environ["translators_default_region"] = "EN"
 
 import translators
 import webvtt
 import yt_dlp
+from webvtt import WebVTT
 
 
-def _get_url_info(url: str):
+def _get_url_info(
+    url: str,
+) -> Dict:
     ydl_opts = {
         "cookiesfrombrowser": ("chrome",),
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        return ydl.extract_info(url, download=False, process=False)
+        return ydl.extract_info(url, download=False)
 
 
-def download_url(url: str):
+def download_url(
+    url: str,
+    foreign_language: str,
+):
     ydl_opts = {
         "writesubtitles": True,
-        "subtitleslangs": ["fr"],
+        "subtitleslangs": [foreign_language],
         "writeautomaticsub": True,
         "cookiesfrombrowser": ("chrome",),
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-def translate_subtitles(fname: str, delimiter="\n"*3):
+
+def translate_subtitles(
+    fname: str,
+    foreign_language: str,
+    native_language: str,
+) -> WebVTT:
+    delimiter = "\n" * 3
     subtitles = webvtt.read(fname)
     subtitle_texts = [caption.text.replace("&nbsp;", "") for caption in subtitles]
 
     translated = []
     for chunk in _chunk_subtitles(subtitle_texts, delimiter):
         merged_chunk = delimiter.join(chunk)
-        translated_chunk = translators.translate_text(merged_chunk, translator="google", from_language="fr")
+        translated_chunk = translators.translate_text(
+            merged_chunk,
+            translator="google",
+            from_language=foreign_language,
+            to_language=native_language,
+        )
         translated += translated_chunk.split(delimiter)
 
     assert len(subtitles) == len(translated)
-    for (caption, new_text) in zip(subtitles, translated):
+    for caption, new_text in zip(iter(subtitles), translated):
         caption.lines = new_text.splitlines()
 
     return subtitles
 
 
-def _chunk_subtitles(subtitle_texts, delimiter):
+def _chunk_subtitles(
+    subtitle_texts: Iterable[str],
+    delimiter: str,
+):
     chunk, char_count = [], 0
     for txt in subtitle_texts:
-        if char_count + len(txt) + (len(chunk) - 1) * len(delimiter) > translators.server.GoogleV2().input_limit:
+        if (
+            char_count + len(txt) + (len(chunk) - 1) * len(delimiter)
+            > translators.server.GoogleV2().input_limit
+        ):
             yield chunk
             chunk, char_count = [], 0
         chunk.append(txt)
@@ -58,47 +82,76 @@ def _chunk_subtitles(subtitle_texts, delimiter):
         yield chunk
 
 
-def main(url: str):
+def watch(
+    url: str,
+    foreign_language: str,
+    native_language: str = "en",
+):
+    """
+    Download, double subtitle, and watch a video at specified url.
 
-    vid_dir = _get_all_files(url)
+    Args:
+        url: youtube URL to display with two sets of subtitles.
+        foreign_language: Language e.g. "fr"
+        native_language: Language for subtitles. Currently must be english i.e. "en"
+    """
+
+    vid_dir = _get_all_files(url, foreign_language, native_language)
+    _launch_player(vid_dir, foreign_language, native_language)
+
+
+def _launch_player(
+    vid_dir: Path,
+    foreign_language: str,
+    native_language: str,
+):
     video_path = _get_file_with_filetype(vid_dir, "webm")
     os.chdir(vid_dir)
     with tempfile.NamedTemporaryFile() as fp:
         fp.write(b"UP cycle secondary-sid")
         fp.flush()
-        subprocess.run(f"mpv --sub-files=fr.vtt:en.vtt --secondary-sid=2 --input-conf=\"{fp.name}\" \"{video_path}\"", shell=True)
+        subprocess.run(
+            f'mpv --sub-files={foreign_language}.vtt:{native_language}.vtt --secondary-sid=2 --input-conf="{fp.name}" "{video_path}"',
+            shell=True,
+        )
 
 
-def _get_all_files(url: str):
+def _get_all_files(
+    url: str,
+    foreign_language: str,
+    native_language: str,
+) -> Path:
     curdir = Path(os.getcwd())
     info = _get_url_info(url)
+    assert info["language"] == foreign_language
     video_dir = curdir / "videos" / info["title"]
     video_dir.mkdir(parents=True, exist_ok=True)
     os.chdir(video_dir)
 
-    download_url(url)
-    foreign_subtitles_path = _get_file_with_filetype(video_dir, "fr.vtt")
-    native_subtitles = translate_subtitles(foreign_subtitles_path)
-    native_subtitles_path = video_dir / "en.vtt"
+    download_url(url, foreign_language)
+    foreign_subtitles_path = _get_file_with_filetype(
+        video_dir, f"{foreign_language}.vtt"
+    )
+    native_subtitles = translate_subtitles(
+        foreign_subtitles_path, foreign_language, native_language
+    )
+    native_subtitles_path = video_dir / f"{native_language}.vtt"
     native_subtitles.save(str(native_subtitles_path))
 
-    foreign_subtitles_path.rename(video_dir / "fr.vtt")
+    foreign_subtitles_path.rename(video_dir / f"{foreign_language}.vtt")
 
     return video_dir
 
 
-def _get_file_with_filetype(dir_path: Path, filetype: str):
+def _get_file_with_filetype(
+    dir_path: Path,
+    filetype: str,
+) -> str:
     files = list(dir_path.glob(f"*.{filetype}"))
     assert len(files) == 1
     return files[0]
 
 
-def _extract_id_from_path(url: str):
-    pattern = r"youtube\.com\/watch\?v=(.*?)(&.*)?$"
-    id_ = re.search(pattern, url).group(1)
-    return id_
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_url = "https://www.youtube.com/watch?v=KFv8aqRdlqk"
-    main(test_url)
+    watch(test_url, "fr")
